283a9d895e217f812b5e6fa67357b32f8f90b471
daniva <daniva@gmail.com>
2010-09-16 23:50:18 +0000

    all

--- scf/settings/settings.py
+++ scf/settings/settings.py
@@ -1,4 +1,4 @@
 ﻿PYTHON_26_DIR = r'c:\python26'
-SCF_DIR = r'C:\projects\svn\danapeerlab\scf'
-MATLAB_PATH = r'C:\Program Files\MATLAB\R2010a'
+SCF_DIR = r'/Users/dv2250/svn/danapeerlab/scf'
+MATLAB_PATH = r'/Applications/MATLAB_R2010a.app/'
 MATLAB_VERSION=2010 #7,8,9,10
