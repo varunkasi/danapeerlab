07c5fccf39397891bbae11c06a84a446f2d0f53d
daniva <daniva@1d83b477-9873-7693-5470-41ad54fc7cce>
2012-02-18 12:25:37 +0000

    bug fix

--- freecell/settings/settings.py
+++ freecell/settings/settings.py
@@ -3,8 +3,8 @@ FREECELL_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MATLAB_PATH = r'guess'
 EXPERIMENTS = {
     'AML with T-Sne data' : (
-        os.path.join(os.path.join(FREECELL_DIR), 'aml_tsne', 'aml_tsne.index')
+        os.path.join(os.path.join(FREECELL_DIR), 'aml_tsne', 'aml_tsne.index'),
         ['patient']),
     'Cytof54 erin clustering' : (
-        os.path.join(os.path.join(FREECELL_DIR), 'cytof54_clustered_erin', 'cytof54_clustered_erins.index')
+        os.path.join(os.path.join(FREECELL_DIR), 'cytof54_clustered_erin', 'cytof54_clustered_erins.index'),
         ['marrow', 'cell_type', 'stim'])}
